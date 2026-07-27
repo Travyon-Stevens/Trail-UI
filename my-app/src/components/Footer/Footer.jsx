44e1c67c00c98446af9a4bbf3421411c3f32b8ef
Travyon <treystvns05@gmail.com>
2026-07-27 17:05:02 -0500

    Trail-001: content added to hero and footer sections

---
+++ my-app/src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+function Footer() {
+    return (
+        <div>Hello World</div>
+    )
+}
+
+export default Footer
---
+++ my-app/src/components/HeroSection/HeroSection.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function HeroSection() {
+    
+    return (
+        <section>
+            Hero Content
+        </section>
+    )
+
+}
+
+export default HeroSection;

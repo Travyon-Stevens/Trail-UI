fd172ac3acc1226d3f1e56da77dfcf5dd2a3b12a
Travyon <treystvns05@gmail.com>
2026-08-13 17:08:43 -0500

    Trail-002: 08/13/26 restructuring

--- my-app/src/components/BudgetPreview/BudgetPreview.jsx
+++ my-app/src/components/BudgetPreview/BudgetPreview.jsx
@@ -1,42 +1,22 @@
 import React from "react";
 import "./BudgetPreview.css";
 
-function BudgetPreview() {
 
-    const budgetCategories = [
-        {
-            id: 1,
-            name: "Necessities",
-            spent: 400,
-            total: 1000
-        },
-        {
-            id: 2,
-            name: "Wants",
-            spent: 200,
-            total: 500
-        },
-        {
-            id: 3,
-            name: "Savings",
-            spent: 350,
-            total: 800
-        }
-    ]
+function BudgetPreview() {
 
     return (
         <article className="budget-preview">
-            <h3>Amount:</h3>
-            <p>$400 / $1,000</p>
 
-            <h3>Remaining:</h3>
-            <p>$600.00</p>
+            <h2>Amount:</h2>
+
+            <p>$400 / $1,000</p>
 
             <div>
                 <button>Edit</button>
                 <button>Delete</button>
                 <button>Details</button>
             </div>
+
         </article>
     )
 }
